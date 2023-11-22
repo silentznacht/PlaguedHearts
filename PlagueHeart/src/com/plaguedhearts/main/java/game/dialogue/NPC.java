b58bcc6c5abddd55ab7b8e2a3cfd9784260e719d
Kevin Escoto <mr.beastlysupreme@gmail.com>
2023-11-22 00:23:33 -0500

    implemented new flow within tavern job, mini game next

--- PlagueHeart/src/com/plaguedhearts/main/java/game/dialogue/NPC.java
+++ PlagueHeart/src/com/plaguedhearts/main/java/game/dialogue/NPC.java
@@ -3,10 +3,12 @@ package main.java.game.dialogue;
 import java.util.Random;
 
 public class NPC {
-    public static String[] characters = {"Sir Reginald", "Lady Elara", "Barkeep", "Bard"};
-    public static String[] locations = {"in a dimly lit tavern", "by the roaring fireplace", "under the flickering candlelight"};
-    public static String[] actions = {"says", "asks", "shouts", "whispers"};
+    private Random random;
+    public static String[] characters = {"Sir Reginald", "Lady Elara", "Barkeep", "Bard", "Winston", "Minerva", "Beatrix", "Severus"};
+    public static String[] locations = {"in a dimly lit tavern", "by the roaring fireplace", "under the flickering candlelight", "under your mums tits"};
+    public static String[] actions = {"says", "asks", "shouts", "whispers", "whales", "begs for a"};
     public static String[] drinks = {"Ol'Copper", "Devil's Breath", "Groslux Special", "Nurgle's Nectar", "Pestilence's Promise", "Remedy Rum"};
+
     public static String[] topics = {
         "The dragon's hoard in the nearby cave",
         "The mysterious disappearance of the town's blacksmith",
@@ -14,7 +16,7 @@ public class NPC {
         "The latest rumors from the kingdom's court",
         "The quality of the ale in this tavern"
     };
-    
+
     public String[] drinkQuestions = {
         "What's the finest ale you have on tap?",
         "Do you have any special brews today?",
@@ -25,29 +27,49 @@ public class NPC {
     
      // Questions || Barkeep (why didn't i just initialize this locally?)
 
-     public static String[] questions = {
-        "Which family wields the ability 'ROT' granted by Nurgle?",
-        "What is the unique ability possessed by the Maladictus family?",
-        "What noble family founded the Pestisdictus Hospitals Foundation?",
-        "What kind of discharge do Viruliths release using their 'ROT' ability?",
-        "Who can reverse the chaotic effects caused by the 'FRENZY' ability of the Maladictus family?",
-        "What is the primary focus of the Morbidius family's contributions to Pestisdictus?",
-        "What is the unique power granted by the 'BLIGHT' ability of the Morbidius family?"
-        // Add more questions here...
+    public static String[] questions = {
+        "Which family wielded the perilous ability known as 'ROT'?",
+        "What ability did the Maladictus family inherit?",
+        "What did the Morbidius family found during the early days of Groslux?",
+        "What did the ability 'BLIGHT' enable the Morbidius clan to do?"
     };
 
     public static String[] answers = {
         "Virulith",
         "FRENZY",
-        "Morbidius",
-        "Infectious diseases causing rapid decay",
-        "Few individuals from the Morbidius family",
-        "Advancing medicine and human biology, founding hospitals",
-        "Transmuting living organisms with infectious bacteria"
-        // Add corresponding answers here...
+        "Pestisdictus Hospitals Foundation",
+        "Transmute living organisms and heal"
     };
 
-    private Random random;
+    
+    public static String [] mixBookAnswera = {
+        "Ol'Copper: Green + Grey",
+        "Devil's Breath: Orange + Purple", 
+        "Groslux Special: Green + Red", 
+        "Nurgle's Nectar: Green + Blue", 
+        "Pestilence's Promise: Black + Green", 
+        "Remedy Rum: Brown + Yellow"
+    };
+    public static String [] mixBook = {
+        "Ol'Copper: Green + Grey",
+        "Devil's Breath: Orange + Purple", 
+        "Groslux Special: Green + Red", 
+        "Nurgle's Nectar: Green + Blue", 
+        "Pestilence's Promise: Black + Green", 
+        "Remedy Rum: Brown + Yellow"
+    };
+
+    public static String[] drinkRelatedInsults = {
+        "Thou shalt sup on naught but water from a stagnant pond!",
+        "Verily, thou art denied the nectar of thine desire!",
+        "Ye shall thirst like a parched desert wanderer!",
+        "Thou art deprived of the sweet ambrosia that quenches!",
+        "Mayest thou sip only from the chalice of disappointment!",
+        "Thou shalt taste the bitter draught of denial!",
+        "Thine cup runneth empty, forsooth!",
+        "Denied the brew of choice, thou art left to languish!"
+    };
+    
 
     public NPC() {
         random = new Random();
--- PlagueHeart/src/com/plaguedhearts/main/java/game/navagation/thepissininn/PissinInn.java
+++ PlagueHeart/src/com/plaguedhearts/main/java/game/navagation/thepissininn/PissinInn.java
@@ -1,12 +1,10 @@
 package main.java.game.navagation.thepissininn;
 
-import static org.junit.Assert.fail;
-
 import java.util.*;
 import main.java.game.dialogue.NPC;
 import main.java.gamesys.Bank;
 
-public class PissinInn {
+public class PissinInn extends Bank {
     /*
      *              Day Quota
      *       Attend to 5 customers, and reach 100 coins by the end of day 1
@@ -24,6 +22,11 @@ public class PissinInn {
      * TODO: Use switch case to sort out user input
      */
     
+     public PissinInn(int userBalance, int userCredits, int userPoints, int userOverallBalance) {
+        super(userBalance, userCredits, userPoints, userOverallBalance);
+        //TODO Auto-generated constructor stub
+     }
+    
      // Prices 
      private static int olCopperPrice = 4;
      private static int devilsBreathPrice = 6;
@@ -40,7 +43,10 @@ public class PissinInn {
      private static String [] actions = NPC.actions;
      private static String [] questions = NPC.questions;
      private static String [] answers = NPC.answers;
-
+     private static String [] mixBook = NPC.mixBook;
+     private static String [] mixBookAnswers = NPC.mixBookAnswera;
+     private static String [] insults = NPC.drinkRelatedInsults;
+ 
      // Profits || Currency
      private static int balance = Bank.userBalance;
      private static int coinEarned = 0;
@@ -64,6 +70,7 @@ public class PissinInn {
                 pesilencePromisePrice, remedyRumPrice
             };
 
+
             String [] drink = NPC.drinks;
             while(k <= drink.length) {if (k >= drinks.length) break; System.out.println("[Drink]: " + drink[k].trim() + " (Price): " + drinksPrices[k]); k++;}
         
@@ -73,47 +80,65 @@ public class PissinInn {
 
     }
 
-    static void tavernJob(int custCount) { // TODO: Allows user to give customers requested drinks, and earn coin
-       int randomIndex = random.nextInt(questions.length);
-       int questCount = 0;
-       int dayCount = 0;
-       int dexCount = 0;
-       int mx = (int) Integer.MAX_VALUE;
-
-       String customers = PissinInn.customerDialogue();
-       String currentQuestion = questions[randomIndex];
-       String answer = answers[randomIndex];
-
-       boolean cycle = false;
-       boolean questionForward = false; // will enable when user chooses to leave activity or not
-       //tavernScan.nextLine();   
-
-            while (dayCount <= mx) {
-                    dayCount++; // days in progress ( count )
-                    while (!cycle) { // TODO: come up with a game for user 
-                        custCount ++;
-                        dexCount++;
-                        System.out.println("Customer (" + custCount + ") " + customers);
-                        System.out.println("\n          [Mini Game]\n\n");
-                            for (int i = 0; i < answers.length; i++) { // Outputs answer choices
-                                questCount ++; // counts amount of quests attempted ie for probablity rate later
-                                System.out.println("[Question]: " + " (" + currentQuestion + ")");
-                                System.out.println(answers[i]);
-                                int userChoice = tavernScan.nextInt();
-                                int answerChosen = userChoice;
-
-
-                                        if (answerChosen != dexCount) { // LOGIC: Answer doesn't match Question index -> wrong ? right
-                                            System.out.println("[Alert: Sorry Incorrect Answer]");
-                                        } else {
-                                            System.out.println("[HAZRAAARRRJRJR]");
-                                        }
-                            }
-                    } 
+    static void tavernJob() { // Handles tavern job -> sends user to handle drinks, and display drinks
+        System.out.println("\n        [Mini Game]\n\n");
+        Scanner tavernScan = new Scanner(System.in);
+        
+        boolean servingDrinks = true;
+    
+        while (servingDrinks) {
+            System.out.println("[ORDER]: " + PissinInn.customerDialogue());
+            System.out.println("[Answer(Mix) -> (What drink did the customer ask for?) Use H for help]: ");
+    
+            String userAnswer = tavernScan.nextLine();
+    
+            if (userAnswer.equalsIgnoreCase("help") || userAnswer.equalsIgnoreCase("h")) { // help menu
+                displayDrinkMenu();
+            } else { 
+                handleDrinkOrder(userAnswer);
             }
-           
+    
+            System.out.println("Do you want to continue serving drinks (Y/N)?");
+            String continueOption = tavernScan.nextLine();
+            if (!continueOption.equalsIgnoreCase("Y")) { // means continueOption stays false when user input is anything other than "Y", thus ending the loop
+                servingDrinks = false;                                 // Y -> exit (stop serving drinks)
+            }
+        }
+    
+        tavernScan.close();
     }
+    
+    static void displayDrinkMenu() {
+        System.out.println("\n        [Mix Book: Drinks]\n");
+        for (String drink : drinks) {
+            System.out.println("(" + drink + ")");
+        }
+    }
+    
+    static void handleDrinkOrder(String userAnswer) {
+        boolean drinkFound = false;
+    
+        for (String drink : drinks) { // if the user matches the customers requested drink then it will move onto the tavern mini game 
+            if (userAnswer.equalsIgnoreCase(drink)) {
+                drinkFound = true;
+                System.out.println("[Now Enter The Proper Mix For " + drink + " (Use H for help)]: ");
+                // Implement logic for mixing the drink here
+                tavernMiniGame();
+                break;
+            }
+        }
+    
+        if (!drinkFound) {
+            System.out.println("\n[Alert: Sorry Invalid Answer]\n");
+        }
+    }
+    
+    static void tavernMiniGame() {
+        // Implement your mini-game logic here
 
+        
+    }
+    
     static void sellTrans() {
 
     }
@@ -142,23 +167,20 @@ public class PissinInn {
                     tranHandler();
                     loopCheck = true;
                     break;
-
                 case 2:
-
+                    //
                 case 3:
-                    tavernJob(0);
-
+                    tavernJob();
+                    break;
                 case 4:
                     System.exit(0);
-
-
             }
 
 
 
     }
      public static void main(String [] args) { 
-        tavernJob(0);
+            tavernJob();
      }
 
 }
